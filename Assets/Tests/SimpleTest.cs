398c6d70f3bddc3a3c8b51dc147ef93e2c9727b0
aaveshmistry <48035013+aaveshmistry@users.noreply.github.com>
2019-10-21 15:17:38 +1100

    Added Tests

---
+++ Assets/Tests/SimpleTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+
+public class SimpleTest : MonoBehaviour
+{
+    [Test]
+
+    public void NUintTest()
+    {
+        Assert.IsTrue(false, "NUint Test Failed");
+    }
+
+    [UnityTest]
+
+    public IEnumerator UnityTest()
+   {
+        yield return new WaitForFixedUpdate();
+
+        Assert.IsTrue(false, "UnityTestFailed");
+   } 
+}
+
